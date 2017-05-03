612cb01531f48764e3568f6655f6b4e0d3e711b6
Nouman Anjum <nouman92@live.com>
2017-05-03 19:34:25 +0500

    fix dir

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendfile('homepage.html');
-})
+// app.use(express.static());
 
-app.listen(process.env.PORT, function () {
+// app.use('/', express.static(path.join(__dirname + './')));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(express.static(path.join(__dirname,"./")));
+
+app.get("/", function(req, res)
+{
+    res.sendFile("homepage.html", {"root": __dirname+"/public"});
+});
+// app.get('/', function (req, res) {
+//   res.sendfile('homepage.html');
+// })
+
+app.listen(process.env.PORT||3000, function () {
   console.log('listening on port '+process.env.PORT)
 })
